426b361bdb849a3560396cb2b6b4cac48e4fedfa
Vilmar Neto <vilmar.neto@gmail.com>
2018-05-15 22:54:01 -0300

    Changed names that are mispelled

--- include/Story.h
+++ include/Story.h
@@ -1,5 +1,3 @@
-#include "../include/Story.h"
-
 
 using namespace std;
 void Story::introduction(){
--- src/Story.cpp
+++ src/Story.cpp
@@ -1,7 +1,15 @@
-#include "../include/Story.h"
-
-
-using namespace std;
-void Story::introduction(){
-
-}
+#include "../include/Story.c"
+#include <stdio.h>
+class Story{
+    private:
+        void introduction();
+        {
+            cout << "The wind of the desert flows..." << endl;
+            getchar();
+            cout << "Saturn reaches the ground. He's crawling on the sand. Waiting for some miracle." << endl;
+            getchar();
+            cout << "So he reaches his backpack, drinking the last bottle of water that he had." << endl;
+            getchar();
+            cout << "He thought it was his end. Until he saw a mirror. It was a strange mirror in the sands."
+        }
+};
